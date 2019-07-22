4abb11da024378d7bc0136dfb4bbecc85b11343e
James Devereux <devereuxjj@gmail.com>
2019-07-22 12:44:38 +0900

    recorder working and appending new audio elements to dom

--- app/javascript/packs/recorder.js
+++ app/javascript/packs/recorder.js
@@ -1,209 +1,167 @@
-//= require rails-ujs
-//= require_tree .
-// set up basic variables for app
-import WaveSurfer from 'wavesurfer.js';
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
 
-var record = document.querySelector('.record');
-var stop = document.querySelector('.stop');
-var deleteButton = document.querySelector('.delete');
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
 
-deleteButton.disabled = true;
+// shim for AudioContext when it's not avb. 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext //audio context to help us record
 
-var soundClips = document.querySelector('#sound-clips');
-var waveform = document.querySelector('#waveform')
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var pauseButton = document.getElementById("pauseButton");
 
-// var mainSection = document.querySelector('.main-controls');
-// var waveform_index = 1;
-// disable stop button while not recording
+//add events to those 2 buttons
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+pauseButton.addEventListener("click", pauseRecording);
 
-stop.disabled = true;
+function startRecording() {
+	console.log("recordButton clicked");
 
-let recording = false;
-
-// visualiser setup - create web audio api context and canvas
-
-//main block for doing the audio recording
-
-if (navigator.mediaDevices.getUserMedia) {
-  console.log('getUserMedia supported.');
-
-  var constraints = { audio: true };
-  var chunks = [];
-
-  var onSuccess = function(stream) {
-    var mediaRecorder = new MediaRecorder(stream);
-
-    record.onclick = function() {
-      mediaRecorder.start();
-      console.log(mediaRecorder.state);
-      console.log("recorder started");
-
-      let recording = !recording;
-      let pauseRecordingIcon = '<i class="fas fa-pause"></i>'
-        if (recording) {
-          record.innerHTML = pauseRecordingIcon; 
-        } else {
-          record.innerHTML = '<i class="fas fa-microphone"></i>';
-        };
-
-
-
+	/*
+		Simple constraints object, for more advanced audio features see
+		https://addpipe.com/blog/audio-constraints-getusermedia/
+	*/
     
-      record.classList.add('playing');
-      stop.disabled = false;
-      record.disabled = true;
-    }
+    var constraints = { audio: true, video:false }
 
-    stop.onclick = function() {
-      mediaRecorder.stop();
-      console.log(mediaRecorder.state);
-      console.log("recorder stopped");
-      record.style.background = "";
-      record.style.color = "";
-      // mediaRecorder.requestData();
+ 	/*
+    	Disable the record button until we get a success or fail from getUserMedia() 
+	*/
 
-      recording = false;
-      if (recording) {
-        record.innerHTML = pauseRecordingIcon; 
-      } else {
-        record.innerHTML = '<i class="fas fa-microphone"></i>';
-      };
-      stop.disabled = true;
-      record.disabled = true;
-    }
+	recordButton.disabled = true;
+	stopButton.disabled = false;
+	pauseButton.disabled = false
 
-    mediaRecorder.onstop = function(e) {
-      console.log("data available after MediaRecorder.stop() called.");
+	/*
+    	We're using the standard promise based getUserMedia() 
+    	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+	*/
 
-      // var clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
-      // console.log(clipName);
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
-      // create a clip container to store wavesurfer object and delete button
-      // var clipContainer = document.createElement('div');
-      // clipContainer.id = 'waveform';
-      // increment waveform_index for the next one
-      // waveform_index ++;
-      // create a delete button
+		/*
+			create an audio context after getUserMedia is called
+			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
+			the sampleRate defaults to the one set in your OS for your playback device
+		*/
+		audioContext = new AudioContext();
 
-      let playing = false;
+		//update the format 
+		// document.getElementById("formats").innerHTML="Format: 1 channel pcm @ "+audioContext.sampleRate/1000+"kHz"
 
-      var buttonControls = document.createElement('div');
-      buttonControls.classList.add('button-controls');
+		/*  assign to gumStream for later use  */
+		gumStream = stream;
+		
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
 
-      var playButton = document.createElement('button');
-      playButton.classList.add('play');
-      let playIcon = '<i class="fas fa-play"></i>'
-      let pauseIcon = '<i class="fas fa-pause"></i>'
-      playButton.innerHTML = playIcon;
+		/* 
+			Create the Recorder object and configure to record mono sound (1 channel)
+			Recording 2 channels  will double the file size
+		*/
+		rec = new Recorder(input,{numChannels:1})
 
+		//start the recording process
+		rec.record()
 
-      var rewindButton = document.createElement('button');
-      rewindButton.classList.add('rewind');
-      let backwardIcon = '<i class="fas fa-backward"></i>'
-      rewindButton.innerHTML = backwardIcon;
+		console.log("Recording started");
 
-      var forwardButton = document.createElement('button');
-      forwardButton.classList.add('forward');
-      let forwardIcon = '<i class="fas fa-forward"></i>'
-      forwardButton.innerHTML = forwardIcon;
+	}).catch(function(err) {
+      //enable the record button if getUserMedia() fails
+      console.log('there was an error', err)
+    	recordButton.disabled = false;
+    	stopButton.disabled = true;
+    	pauseButton.disabled = true
+	});
+}
 
-      // append the delete button to the clip container
-      buttonControls.appendChild(rewindButton);
-      buttonControls.appendChild(playButton);
-      buttonControls.appendChild(forwardButton);
+function pauseRecording(){
+	console.log("pauseButton clicked rec.recording=",rec.recording );
+	if (rec.recording){
+		//pause
+		rec.stop();
+		pauseButton.innerHTML="Resume";
+	}else{
+		//resume
+		rec.record()
+		pauseButton.innerHTML="Pause";
 
-      deleteButton.disabled = false;
+	}
+}
 
-      // append clipContainer to the dom in soundclips
-      // soundClips.appendChild(clipContainer);
+function stopRecording() {
+	console.log("stopButton clicked");
 
-      // var currentContainer = `#waveform-${waveform_index}`
-      // create the wavesurfer object in the clip-container
-      var wavesurfer = WaveSurfer.create({
-        container: '#waveform',
-        waveColor: '#5CD1B7',
-        progressColor: '#3c8977',
-        skipLength: 5.0
-    });
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+	pauseButton.disabled = true;
 
-      // if(clipName === null) {
-      //   clipLabel.textContent = 'My unnamed clip';
-      // } else {
-      //   clipLabel.textContent = clipName;
-      // }
+	//reset button just in case the recording is stopped while paused
+	pauseButton.innerHTML="Pause";
+	
+	//tell the recorder to stop the recording
+	rec.stop();
 
-      // clipContainer.appendChild(audio);
+	//stop microphone access
+	gumStream.getAudioTracks()[0].stop();
 
-      // audio.controls = true;
-      var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
-      chunks = [];
-      var audioURL = window.URL.createObjectURL(blob);
-      // audio.src = audioURL;
-      console.log("recorder stopped");
-      waveform.appendChild(buttonControls);
+	//create the wav blob and pass it on to createDownloadLink
+	rec.exportWAV(createDownloadLink);
+}
 
-      wavesurfer.load(audioURL);
+function createDownloadLink(blob) {
+	
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+	var li = document.createElement('li');
+	var link = document.createElement('a');
 
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toISOString();
 
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
 
-      playButton.onclick = function(e) {
-        playing = !playing;
-        if (playing) {
-          playButton.innerHTML = pauseIcon; 
-        } else {
-          playButton.innerHTML = playIcon;
-        };
-        wavesurfer.playPause();
-      }
+	//save to disk link
+	link.href = url;
+	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.innerHTML = "Save to disk";
 
-      wavesurfer.on('finish', function() {
-        playButton.innerHTML = playIcon;
-        playing = false;
-      })
+	//add the new audio element to li
+	li.appendChild(au);
+	
+	//add the filename to the li
+	li.appendChild(document.createTextNode(filename+".wav "))
 
-      rewindButton.onclick = function(e) {
-        wavesurfer.skipBackward();
-      }
+	//add the save to disk link to li
+	li.appendChild(link);
+	
+	//upload link
+	var upload = document.createElement('a');
+	upload.href="#";
+	upload.innerHTML = "Upload";
+	upload.addEventListener("click", function(event){
+		  var xhr=new XMLHttpRequest();
+		  xhr.onload=function(e) {
+		      if(this.readyState === 4) {
+		          console.log("Server returned: ",e.target.responseText);
+		      }
+		  };
+		  var fd=new FormData();
+		  fd.append("audio_data",blob, filename);
+		  xhr.open("POST","upload.php",true);
+		  xhr.send(fd);
+	})
+	li.appendChild(document.createTextNode (" "))//add a space in between
+	li.appendChild(upload)//add the upload link to li
 
-      forwardButton.onclick = function(e) {
-        wavesurfer.skipForward();
-      }
-
-      deleteButton.onclick = function(e) {
-        // console.log(e.target.parentNode)
-        // e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-        waveform.remove();
-        record.disabled = false;
-        var clipContainer = document.createElement('div');
-        clipContainer.id = 'waveform';
-        soundClips.appendChild(clipContainer);
-        waveform = document.querySelector('#waveform');
-      }
-
-      // clipLabel.onclick = function() {
-      //   var existingName = clipLabel.textContent;
-      //   var newClipName = prompt('Enter a new name for your sound clip?');
-      //   if(newClipName === null) {
-      //     clipLabel.textContent = existingName;
-      //   } else {
-      //     clipLabel.textContent = newClipName;
-      //   }
-      // }
-    }
-
-    // mediaRecorder.onstop.bind(waveform_index)
-
-    mediaRecorder.ondataavailable = function(e) {
-      chunks.push(e.data);
-    }
-  }
-
-  var onError = function(err) {
-    console.log('The following error occured: ' + err);
-  }
-
-  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
-
-} else {
-   console.log('getUserMedia not supported on your browser!');
+	//add the li element to the ol
+  recordingsList.appendChild(li);
 }
