6c327ee8d91fd13f78f529bce0697d840c81232b
James Devereux <devereuxjj@gmail.com>
2019-08-08 19:56:25 +0100

    created sign up form but choosing role doesnt work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users do 
-    get '/users/sign_out' => 'devise/sessions#destroy'
   end
   
   root to: 'pages#home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_085409) do
+ActiveRecord::Schema.define(version: 2019_08_08_132331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2019_07_25_085409) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.string "photo"
+    t.string "name"
+    t.boolean "teacher"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
